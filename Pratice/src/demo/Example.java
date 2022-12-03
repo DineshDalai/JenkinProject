eac64246e26b2570fe0a18b38838098163bd3189
Dinesh <dineshdalai03@gmail.com>
2022-12-03 20:28:26 +0530

    code pratice

---
+++ Pratice/src/demo/Example.java
@@ -0,0 +1,27 @@
+package demo;
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Example {
+
+	public static void main(String[] args) {
+		
+		
+WebDriver driver;
+		
+		
+	    WebDriverManager.chromedriver().setup();
+	    
+	    driver = new ChromeDriver();
+	    
+	    driver.manage().window().maximize();
+	    
+	    driver.get("https://www.google.com/");
+	}
+
+}

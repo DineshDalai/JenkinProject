d5314e45bcee36726ceaf6d00eed4236acd298bb
Dinesh <dineshdalai03@gmail.com>
2024-04-21 19:49:16 +0530

    project

---
+++ XYZ/src/main/java/XYZ/XYZ/App.java
@@ -0,0 +1,18 @@
+package XYZ.XYZ;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	WebDriver driver = new ChromeDriver();
+    	driver.get("https://www.google.com/");
+        System.out.println( "Hello World! Dinesh" );
+    }
+}
